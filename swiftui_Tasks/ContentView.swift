09b2d29c27a6f2f9741c23df38250abb43453294
Kalai Selvi <kalaiselvi@Kalai-Mac-Book-Pro.local>
2024-02-19 14:11:19 +0530

    intial

---
+++ swiftui_Tasks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swiftui_Tasks
+//
+//  Created by Kalai Selvi on 19/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftui_Tasks/swiftui_TasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_TasksApp.swift
+//  swiftui_Tasks
+//
+//  Created by Kalai Selvi on 19/02/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_TasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
